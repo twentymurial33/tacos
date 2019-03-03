3ac927f9ac93a57d5e769585ac6cd15e8c468135
Murial Anindo <twentymurial@outlook.com>
2019-03-03 08:05:47 -0500

    move to master

---
+++ tacos.js
@@ -0,0 +1,25 @@
+const classmates =["Christian","CJ","matthew"];
+
+const wrapper=document.getElementById("wrapper");
+
+for(let i=0; i< classmates.length;i++){
+    const classmate=classmates[i];
+
+    const button=document.createElement("button");
+    button.innerHTML="How many tacos did"+classmate + "eat?"
+
+    button.addEventListener("click",function(){
+           const tacoCount=i*2;
+
+           alert(classmate +"ate")
+
+           if (tacoCount > 3) {
+            alert("That's a lot of tacos, " + classmate + ".\nTaco 'Bout Hungry!");
+          }
+        });
+      
+        wrapper.appendChild(button);
+      }
+      
+
+
